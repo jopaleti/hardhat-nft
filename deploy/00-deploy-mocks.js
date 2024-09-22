078652a9e11deb3a13c0620ed989fec0f88e1f16
jopaleti <oopaletijohnson@gmail.com>
2024-09-22 07:41:36 +0100

    ...

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -18,11 +18,11 @@ module.exports = async function (hre) {
             log: true,
             args: [BASE_FEE, GAS_PRICE_LINK],
         });
-        // await deploy("MockV3Aggregator", {
-        //     from: deployer,
-        //     log: true,
-        //     args: [DECIMALS, INITIAL_PRICE]
-        // })
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE]
+        });
 
         log("Mocks Deployed!");
         log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
